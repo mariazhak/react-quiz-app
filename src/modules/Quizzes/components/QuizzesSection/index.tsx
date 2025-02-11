454d52081cd1262afffdfc596e78d18edfc5cc8d
mariazhak <mariia.zhak@auk.edu.ua>
2025-02-11 21:44:08 +0200

    sockets

--- src/modules/Quizzes/components/QuizzesSection/index.tsx
+++ src/modules/Quizzes/components/QuizzesSection/index.tsx
@@ -1,14 +1,43 @@
 import { Box, Typography } from '@mui/material';
-import { FC, memo, useEffect } from 'react';
+import { FC, memo, useEffect, useState } from 'react';
 import { QuizCard } from '../../../../components/QuizCard';
 
 import { styles } from './styles';
 import { useQuizzesData } from '../../hooks/useQuizzesData';
+import { socketUrl } from 'src/utils';
 
 export interface QuizzesSectionProps {}
 
 export const QuizzesSection: FC<QuizzesSectionProps> = memo(() => {
   const { quizzes, fetchQuizzes } = useQuizzesData();
+
+  const [logs, setLogs] = useState<string[]>([]);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket(`ws://${socketUrl}/ws`);
+
+    ws.onopen = () => console.log('Connected to WebSocket logs');
+    
+    ws.onmessage = (event) => {
+      const logMessage = event.data;
+      setLogs((prevLogs) => [...prevLogs, logMessage]);
+
+      if (logMessage.includes('Updated')) {
+        void fetchQuizzes();
+        socket?.send('Clear');
+      }
+    };
+
+    ws.onerror = (error) => console.error('WebSocket error:', error);
+    ws.onclose = () => console.log('WebSocket closed');
+
+    setSocket(ws);
+
+    return () => {
+      ws.close();
+    };
+  }, []);
 
   useEffect(() => {
     void fetchQuizzes();
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+export const socketUrl = import.meta.env.VITE_SOCKET_URL;
